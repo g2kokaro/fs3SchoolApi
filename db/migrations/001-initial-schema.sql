aaa80d2a409d086a0d408b38c683a0954f98a94d
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-04-11 15:00:44 -0400

    Added teachers, classes, and student_classes tables to initial schema.

--- db/migrations/001-initial-schema.sql
+++ db/migrations/001-initial-schema.sql
@@ -1,5 +1,41 @@
+-- Up
 CREATE TABLE students (
   id INTEGER PRIMARY KEY,
   firstname TEXT,
   lastname TEXT
 );
+
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY,
+  firstname TEXT,
+  lastname TEXT
+);
+
+CREATE TABLE classes (
+  id INTEGER PRIMARY KEY,
+  code TEXT,
+  name TEXT,
+  teacher_id INTEGER,
+  startDate: TEXT,
+  endDate: TEXT,
+  CONSTRAINT classes_fk_teacher_id 
+    FOREIGN KEY REFERENCES teachers (id)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE student_classes (
+  class_id INTEGER,
+  student_id INTEGER,
+  CONSTRAINT student_classes_fk_class_id
+    FOREIGN KEY REFERENCES classes (id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT student_classes_fk_student_id
+    FOREIGN KEY REFERENCES students(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+)
+
+-- Down
+DROP TABLE students;
+DROP TABLE teachers;
+DROP TABLE classes;
+DROP TABLE student_classes;
