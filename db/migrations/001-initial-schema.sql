799ed2c895afe9918499d7338d30691a08fe9838
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-04-18 21:38:19 -0400

    Fixed classes_fk_teacher_id constraint. Removed dummy data from database.

--- db/migrations/001-initial-schema.sql
+++ db/migrations/001-initial-schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE classes (
   end_date TEXT,
   CONSTRAINT classes_fk_teacher_id 
     FOREIGN KEY (teacher_id) REFERENCES teachers(id)
-    ON UPDATE CASCADE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE student_classes (
@@ -33,33 +33,6 @@ CREATE TABLE student_classes (
     FOREIGN KEY (student_id) REFERENCES students(id)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-insert into teachers (first_name, last_name) values 
-  ('David the Dementor', 'Krett');
-
-insert into teachers (first_name, last_name) values ('Tim', 'Williams');
-
-insert into classes (code, name, teacher_id, start_date, end_date) values 
-  ('BCDV1006', 'Full Stack Development I', 1, '2019-01-01', '2019-01-31');
-
-insert into classes (code, name, teacher_id, start_date, end_date) values 
-  ('BCDV1007', 'Full Stack Development II', 2, '2019-02-01', '2019-02-28');
-
-insert into classes (code, name, teacher_id, start_date, end_date) values 
-  ('BCDV1008', 'Full Stack Development III', 1, '2019-03-01', '2019-03-31');
-
-insert into students (first_name, last_name) values ('Dima', 'Kokarovtsev');
-insert into students (first_name, last_name) values ('Student', 'Two');
-insert into students (first_name, last_name) values ('Classmate', 'Three');
-
-insert into student_classes values (1, 1);
-insert into student_classes values (2, 1);
-insert into student_classes values (3, 1);
-insert into student_classes values (2, 2);
-insert into student_classes values (2, 3);
-insert into student_classes values (3, 3);
-
-
 
 -- Down
 DROP TABLE students;
